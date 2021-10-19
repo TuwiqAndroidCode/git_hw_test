39c29ab720bca627e62da1a39e7c1d4903d7880d
Afrah <Afrah.suliman.ruh.k@tuwaiq.edu.sa>
2021-10-19 21:08:03 +0300

    HW-Answer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,18 @@
 fun main(args: Array<String>) {
+    //Q1 Part 2
+var meMap = mutableMapOf<String,String>("Dr." to "Amal","Eng." to "Sara","T." to "Ali") //Create a map from 3 elements
+    println(meMap) // For adding to map write
+    meMap.putIfAbsent("Pro","Ahmed") //Add to map
+    println(meMap)
+    meMap["Pro"]="Ragad" //edit the value from the key
+    println(meMap)
+    meMap.remove("T.") // remove  the value T.Ali
+    println(meMap)
 
-    
-    println("Afrah")
 
+   //--------------------------------------------------------------------------
+    //Part 2
+   var meMap2 = mutableMapOf<Int,String>(3 to "Food", 4 to "Soda", 5 to "Snacks") // Create a map from 3 elements
+    meMap2.putAll(listOf("Apple","Juice","Chips").mapIndexed{index, item -> index.toInt() to item}) // Create a list of 3 elements and add it to map
+    println(meMap2)
 }

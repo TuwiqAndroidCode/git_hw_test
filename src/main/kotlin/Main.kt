b96328491ff42b93f3d1b2403d80ffe4b83c6b80
Raghad Abdullah <Raghad.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-19 21:28:00 +0300

    update.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,25 @@
-fun main(args: Array<String>) {
+//Q1 :
+fun main() {
+        var mymap= mutableMapOf("student" to "Raghad" ,"student2" to "Rola","student3" to "maha" )
+        println(mymap)
+    mymap.putIfAbsent("student4" , "Afrah")
+    println(mymap)
+    mymap["student3"] ="mashail"
+    println(mymap)
+    mymap.remove(key= "student")
+    println(mymap)
+
+    //========================================================
+
+    //Q2 :
+    var Cities= mutableMapOf("Cities" to "Riyad" ,"Cities2" to "jeddah","Cities3" to "yanbu" )
+    println(Cities)
+    Cities.putAll(listOf("abha","damam","almadina").mapIndexed{ index, s ->index.toString() to s  })
+    println(Cities)
 
 
-    println("prinbbt your name")
+    /*val numbersMap = mutableMapOf("one" to 1, "two" to 2, "three" to 3) // anathor suliton
+    numbersMap.putAll(listOf("four" to 4, "five" to 5))
+    println(numbersMap)*/
 
 }
